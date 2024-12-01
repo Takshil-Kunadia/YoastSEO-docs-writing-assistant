b62b1e3bb4d71acb7c9d1cf7c46fdc6175240897
Takshil Kunadia <takshilkunadia@gmail.com>
2024-12-01 17:07:05 +0530

    clean up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 /* Written by Takshil Kunadia */
 
-import { getGmailAliases, getGmailLabels } from './gmail';
-import { sendmail } from './server/mail';
 import { doGet } from './server/webapp';
 
-global.sendmail = sendmail;
 global.doGet = doGet;
-
-global.getGmailLabels = getGmailLabels;
-global.getGmailAliases = getGmailAliases;
